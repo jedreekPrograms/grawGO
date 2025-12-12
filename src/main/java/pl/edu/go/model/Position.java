617bba685b69114434b3b033312e9b9e5a8f4861
Jedrek <jedreek323@gmail.com>
2025-12-12 01:35:24 +0100

    klasa Position

---
+++ src/main/java/pl/edu/go/model/Position.java
@@ -0,0 +1,34 @@
+
+import java.util.Objects;
+
+public final class Position {
+    public final int x;
+    public final int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+
+        if (!(o instanceof Position)) return false;
+
+        Position pos = (Position) o;
+
+        return x == pos.x && y == pos.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + "," + y + ")";
+    }
+}
